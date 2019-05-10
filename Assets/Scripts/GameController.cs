6c36116c4d08eaecf986573289bb3a063dea184c
Willem Dehaes <willemdehaes@gmail.com>
2019-05-10 11:34:47 -0400

    add some color!

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+  public float speed = 10.0f;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    HandleSymInput();
+  }
+
+  private void HandleSymInput()
+  {
+    float horizontalMove = Input.GetAxis("Vertical") * speed;
+    float verticalMove = Input.GetAxis("Horizontal") * speed;
+  }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public float speed = 0.1f;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Get the horizontal and vertical axis.
+    // By default they are mapped to the arrow keys.
+    // The value is in the range -1 to 1
+    float xMove = Input.GetAxis("Horizontal") * speed;
+    float zMove = Input.GetAxis("Vertical") * speed;
+
+
+    // Move translation along the object's z-axis
+    transform.Translate(xMove, 0, zMove);
+
+  }
+}

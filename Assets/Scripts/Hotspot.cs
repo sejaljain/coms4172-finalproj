91a78d3e6851acb308c1b0fdb15eec51787bcbe1
Sejal Jain <jain.sejal97@gmail.com>
2019-05-10 15:02:32 -0400

    ARKit

---
+++ Assets/Scripts/Hotspot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hotspot : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void moveToHotspot()
+    {
+
+        player.transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+
+    }
+
+}
---
+++ Assets/Scripts/PositionTracker.cs
@@ -0,0 +1,33 @@
+﻿
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.XR.iOS;
+
+ public class PositionTracker : MonoBehaviour
+{
+
+    public Transform vrCamera;
+    private UnityARSessionNativeInterface m_session;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        m_session = UnityARSessionNativeInterface.GetARSessionNativeInterface();
+        ARKitWorldTrackingSessionConfiguration config = new ARKitWorldTrackingSessionConfiguration();
+        config.planeDetection = UnityARPlaneDetection.Horizontal;
+        config.alignment = UnityARAlignment.UnityARAlignmentGravity;
+        config.getPointCloudData = true;
+        config.enableLightEstimation = true;
+        m_session.RunWithConfig(config);
+    }
+
+    void Update()
+    {
+
+        if (vrCamera != null ) {
+            Matrix4x4 matrix = m_session.GetCameraPose();
+            vrCamera.transform.localPosition = UnityARMatrixOps.GetPosition(matrix);
+        }
+    }
+}
